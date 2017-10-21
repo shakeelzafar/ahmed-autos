04d6c1808782298efa8f8f1f172c4c0e010f3f8a
shakeelzafar <bsef12m511@pucit.edu.pk>
2017-10-21 19:17:26 +0500

    first commit
    goes here

---
+++ dreport/Controllers/reportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dreport.Models;
+using System.Data.Entity;
+
+namespace dreport.Controllers
+{
+    public class reportController : Controller
+    {
+        //
+        // GET: /report/
+        autosEntities ctx = new autosEntities();
+        public ActionResult dashboard()
+        {
+            return View();
+        }
+        public ActionResult products()
+        {
+            return View();
+        }
+        public ActionResult factors()
+        {
+            return View();
+        }
+
+     
+       
+    }
+}
---
+++ dreport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dreport.Startup))]
+namespace dreport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
